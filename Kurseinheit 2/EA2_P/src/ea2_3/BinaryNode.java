00dc2f3e8a0fae1ad3a55894450558a38b8714f7
butovoforever <svechi91@mail.ru>
2020-12-31 05:12:49 +0300

    initial creation of assigment's files about binary tree

---
+++ Kurseinheit 2/EA2_P/src/ea2_3/BinaryNode.java
@@ -0,0 +1,17 @@
+package ea2_3;
+
+class BinaryNode {
+    private BinaryNode leftSon, rightSon;
+    private int value;
+
+    public BinaryNode(int v) {
+        value = v;
+    }
+
+    public boolean contains(int v) { /* s. a) ... */ }
+
+    public void insert(int v) { /* s. b) ... */ }
+
+    public void inorder() { /* s. c) ... */ }
+}
+
