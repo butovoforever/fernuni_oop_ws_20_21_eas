85afbce668108fa2025339e52263fb354e5a55c0
butovoforever <svechi91@mail.ru>
2021-01-02 10:54:59 +0300

    getNumberOfNodes, setNodes, isAdjacent(1) are implemented

--- Kurseinheit 3/EA3_P/src/ea3_1/Graph.java
+++ Kurseinheit 3/EA3_P/src/ea3_1/Graph.java
@@ -13,16 +13,22 @@ public class Graph {
         }
         throw new GraphException();
     }
+
+
     public Graph(int nodeNumber) {
         nodes = new String[nodeNumber];
         adjacencyMatrix = new boolean[nodeNumber][nodeNumber];
     }
+
+
     public Graph(String[] nodes) {
         this(nodes.length);
         for (int i = 0; i < nodes.length; i++) {
             this.nodes[i] = nodes[i];
         }
     }
+
+
     public String getNode(int index) throws GraphException {
         // ...liefert den Namen des Knotens an der
         // durch den Wert des Parameters bezeichneten Position,
@@ -34,28 +40,48 @@ public class Graph {
 
     }
 
-//    public int getNumberOfNodes() {
-//        // ...liefert die Anzahl der Knoten zurueck.
-//
-//    }
-//    public void setNodes(String[] nodes) {
-//        // ...weist den ersten k Elementen des Attributs nodes die
-//        // entsprechenden Werte des Parameters nodes zu. k ist dabei das
-//        // Minimum aus der Laenge des Attributs und des Parameters.
-//    }
-//    public boolean isAdjacent(int index1, int index2) throws GraphException {
-//        // ...liefert "wahr", wenn die Knoten mit Index
-//        // index1 und index2 benachbart sind, sonst "falsch".
-//    }
-//    public boolean isAdjacent(String node1, String node2) throws GraphException {
-//        // ...liefert "wahr", wenn die Laender mit Namen
-//        // node1 und node2 benachbart sind, sonst "falsch".
-//    }
+
+    public int getNumberOfNodes() {
+        // ...liefert die Anzahl der Knoten zurueck.
+        return nodes.length;
+    }
+
+
+    public void setNodes(String[] nodes) {
+        // ...weist den ersten k Elementen des Attributs nodes die
+        // entsprechenden Werte des Parameters nodes zu. k ist dabei das
+        // Minimum aus der Laenge des Attributs und des Parameters.
+        for (int i = 0; i < Integer.min(nodes.length, this.nodes.length); i++) {
+       this.nodes[i] = nodes[i];
+        }
+
+    }
+
+
+    public boolean isAdjacent(int index1, int index2) throws GraphException {
+        // ...liefert "wahr", wenn die Knoten mit Index
+        // index1 und index2 benachbart sind, sonst "falsch".
+        if (adjacencyMatrix[index1][index2] == true & adjacencyMatrix[index2][index1] == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public boolean isAdjacent(String node1, String node2) throws GraphException {
+        // ...liefert "wahr", wenn die Laender mit Namen
+        // node1 und node2 benachbart sind, sonst "falsch".
+    }
+
+
     public void addEdge(int index1, int index2) throws GraphException {
         // ...fuegt Kante zwischen den Knoten mit
         // Index index1 und index2 ein, wenn diese
         // Indizes von Laendern sind; sonst eine GraphException.
     }
+
+
     public void addEdge(String node1, String node2) throws GraphException {
         // ...fuegt Kante zwischen den Knoten mit
         // Laendernamen node1 und node2 ein, wenn diese
--- Kurseinheit 3/EA3_P/src/ea3_1/TestGraph.java
+++ Kurseinheit 3/EA3_P/src/ea3_1/TestGraph.java
@@ -3,13 +3,9 @@ package ea3_1;
 public class TestGraph {
     public static void main(String[] args) throws Exception {
         Graph testingGraph = new Graph(2);
-        testingGraph.nodes[0] = ("a");
-        testingGraph.nodes[1] = ("a");
-        try {
-            testingGraph.getNode(25);
-        } catch (GraphException e) {
-            System.out.println("bad index");
-        }
+
+        testingGraph.getNode(25);
+
 
 
 
