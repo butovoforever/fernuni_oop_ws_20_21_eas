f22ca7c867a504042ff485c38c0d078f0ba4f784
butovoforever <svechi91@mail.ru>
2021-01-03 20:58:36 +0300

    assignment for abstract classes implementation

--- Kurseinheit 3/EA3_P/src/ea3_5/Geometrieobjektverwaltung.java
+++ Kurseinheit 3/EA3_P/src/ea3_5/Geometrieobjektverwaltung.java
@@ -1,16 +1,66 @@
 package ea3_5;
 
-class Geometrieobjekt {
+abstract class Geometrieobjekt {
+
+    public abstract boolean istPunktsymmetrisch();
+    public abstract boolean istGeschlossen();
 
     void druckeEigenschaften(){
-        
+        if (istGeschlossen()) {
+            System.out.println("Ist geschlossen.");
+        } else {
+            System.out.println("Ist nicht geschlossen.");
+        }
+
+        if (istPunktsymmetrisch()) {
+            System.out.println("Ist punktsymmetrisch.");
+        } else {
+            System.out.println("Ist nicht punktsymmetrisch.");
+        }
     }
+
+
+}
+
+class Kreis extends Geometrieobjekt {
+
+    public boolean istPunktsymmetrisch() {
+        return true;
+    }
+
+    public boolean istGeschlossen() {
+        return true;
+    }
+
+}
+
+class Bogenstueck extends Geometrieobjekt {
+
+    public boolean istPunktsymmetrisch() {
+        return false;
+    }
+
+    public boolean istGeschlossen() {
+        return false;
+    }
+
+}
+
+class Rechteck  extends Geometrieobjekt {
+
+    public boolean istPunktsymmetrisch() {
+        return true;
+    }
+
+    public boolean istGeschlossen() {
+        return true;
+    }
+
 }
 
 
-
 class Geometrieobjektverwaltung {
-    //private static void main(String[] args) {
+    public static void main(String[] args) {
         Geometrieobjekt geometrieobjekt = null;
 
         if (args[0].equals("Kreis"))
